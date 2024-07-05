ae2e4ea223531b596d60f6f97b3a904891d867a5
Muhammad Wasif Arslan <66217748+wasif-arslan@users.noreply.github.com>
2024-07-05 18:58:17 +0500

    new

---
+++ components/main/navbar.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { LINKS, NAV_LINKS, SOCIALS } from "@/constants";
+
+export const Navbar = () => {
+  return (
+    <div className="w-full h-[65px] fixed top-0 shadow-lg  backdrop-blur-md bg-[white] z-50 px-10 text-gray-800">
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+        <Link
+          href="#about-me"
+          className="h-auto w-auto flex flex-row items-center"
+        >
+          <Image
+            src="/genverse_logo.png"
+            alt="Logo"
+            width={100}
+            height={100}
+            draggable={false}
+            className="cursor-pointer hover:animate-slowspin"
+          />
+          <div className="font-bold  hidden md:block text-gray-800">
+            Genverse
+          </div>
+        </Link>
+
+        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
+          <div className="flex items-center justify-between w-full h-auto  mr-[15px] px-[20px] py-[10px] rounded-full text-gray-800">
+            {NAV_LINKS.map((link) => (
+              <Link
+                key={link.title}
+                href={link.link}
+                className="cursor-pointer hover:text-[rgb(112,66,248)] transition"
+              >
+                {link.title}
+              </Link>
+            ))}
+
+            {/* source code */}
+            <Link
+              href={LINKS.sourceCode}
+              target="_blank"
+              rel="noreferrer noopener"
+              className="cursor-pointer hover:text-[rgb(112,66,248)] transition"
+            >
+              Source Code
+            </Link>
+          </div>
+        </div>
+
+        <div className="flex flex-row gap-5">
+          {SOCIALS.map(({ link, name, icon: Icon }) => (
+            <Link
+              href={link}
+              target="_blank"
+              rel="noreferrer noopener"
+              key={name}
+            >
+              <Icon className="h-6 w-6 text-gray-800" />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
